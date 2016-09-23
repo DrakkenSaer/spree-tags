73f6e9b964ecf56fa06167308ab05f27e023487c
drakkensaer <shadowdrakkeen@gmail.com>
2016-09-23 19:31:26 +0000

    update

--- app/overrides/decorate_product_show_description.rb
+++ app/overrides/decorate_product_show_description.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: "display_tags_on_products",
-  insert_before: "[data-hook='product_description']",
+  insert_top: "[data-hook='product_description']",
   partial: "spree/shared/tags",
   disabled: false)
