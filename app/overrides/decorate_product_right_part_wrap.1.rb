6d0f3d131b8218462dfe3826da3502464ea0e664
drakkensaer <shadowdrakkeen@gmail.com>
2016-10-03 06:07:11 +0000

    update

--- app/overrides/decorate_product_right_part_wrap.1.rb
+++ app/overrides/decorate_product_right_part_wrap.1.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
-  name: "display_tags_on_products",
+  name: "display_tags_on_product",
   insert_top: "[data-hook='product_right_part_wrap']",
-  partial: "spree/shared/tags",
+  text: "<%= render 'spree/shared/tags', object: @product %>",
   disabled: false)
