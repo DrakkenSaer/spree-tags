9aef9a2d53315495ccee16f85a24b79c1558aece
drakkensaer <shadowdrakkeen@gmail.com>
2016-10-07 01:37:00 +0000

    update

--- db/migrate/20160921210134_create_identities.rb
+++ db/migrate/20160921210134_create_identities.rb
@@ -1,6 +1,6 @@
 class CreateIdentities < ActiveRecord::Migration
   def change
-    create_table :spree_identities do |t|
+    create_table :identities do |t|
       t.belongs_to :tag, index: true, foreign_key: true
       t.belongs_to :product, index: true, foreign_key: true
 
