4d290308fca4f4cde7ec76428d2826129a6d1c7a
drakkensaer <shadowdrakkeen@gmail.com>
2017-04-20 06:20:39 +0000

    update

--- app/models/spree/tagging.rb
+++ app/models/spree/tagging.rb
@@ -1,4 +1,4 @@
 class Spree::Tagging < ActiveRecord::Base
-  belongs_to :tag
+  belongs_to :tag, class_name: 'Spree::Tag'
   belongs_to :taggable, polymorphic: true
 end
--- db/migrate/20170419204821_create_taggings.rb
+++ db/migrate/20170419204821_create_taggings.rb
@@ -2,7 +2,7 @@ class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :spree_taggings do |t|
       t.belongs_to :taggable, index: true, polymorphic: true
-      t.belongs_to :tag, index: true, foreign_key: true
+      t.belongs_to :tag, index: true
 
       t.timestamps null: false
     end
